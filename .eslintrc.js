1dba784553c6a822ec90d3a5ad124e0f44db9cf4
Matthew <m_kmorgan@hotmail.com>
2020-12-02 12:35:22 -0700

    little things here and there

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+xmodule.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
--- database/seed.js
+++ database/seed.js
@@ -2,19 +2,33 @@ const mongoose = require('mongoose');
 let faker = require('faker');
 const db = require('./database.js');
 
+// cleans out Mongodb
+var asyncRemove = () => {
+  return new Promise ((resolve, reject) => {
+    db.remove({}, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  })
+}
+// asyncRemove();
+
 console.log('seeding db ...');
 
 // get images from S3, there are 20 named 0.jpg - 19.jpg
 let numImgs = 20;
 let s3Images = [];
-for (var i = 0; i < numImgs; i++) {
+for (let i = 0; i < numImgs; i++) {
   console.log('linking S3 Images');
   s3Images.push(`https://fec-related-items.s3-us-west-2.amazonaws.com/bars/${i}.jpg`);
 }
 
 
 let seedData = [];
-for (var i = 0; i <= 100; i++) {
+for (let i = 0; i <= 100; i++) {
   var document = {};
   document.name = faker.name.findName();
   document.rating = Math.floor(Math.random() * Math.floor(11));
@@ -22,7 +36,7 @@ for (var i = 0; i <= 100; i++) {
   document.prime = faker.random.boolean();
   document.price = faker.commerce.price();
   document.images = [];
-  for (var j = 0; j < 3; j++){
+  for (let j = 0; j < 3; j++){
     document.images.push(s3Images[Math.floor(Math.random() * numImgs)]);
   }
   seedData.push(document);
